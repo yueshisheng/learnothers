c513cc9c3f49781d236d468d583fca2e5a9ba8d9
yueshisheng <2298672896@qq.com>
2023-04-27 13:49:03 +0800

    strcpy和memcpy函数

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
+
 //模板可变参数
 using namespace std;
 template<typename   ...T>
@@ -30,6 +33,23 @@ int main() {
     C c;
     cout<<c.c_data<<endl;
     cout<<c.data<<endl;//返回虚基类数据
+
+    //strcpy()函数,会遇到\n结束
+    char* src="yueshi\0sheng";
+    char dest[20];
+    strcpy(dest,src);
+    cout<<dest<<endl;
+
+    //memcpy()函数按指定大小copy,遇到\0也会结束
+    char newdest[20];
+    memcpy(newdest,src,100);
+    cout<<newdest<<endl;
+
+    
+
+
+
+
 
     return 0;
 }
